6eee5086d658f0620ff6e0ddef0cd6f826ee44b2
Рожков Артем Евгеньевич <Rozhkov.A.Ev@sberbank.ru>
2021-09-24 22:42:18 +0700

    Add empty spring boot app

---
+++ src/test/java/com/rozhkov/fitness/management/FitnessManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rozhkov.fitness.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
